25852b3a08f7e30de9509bca11c13e6a42013b5d
mejiasimon <97535209+mejiasimon@users.noreply.github.com>
2023-11-07 10:08:26 -0500

    final

--- models/modeloCar.js
+++ models/modeloCar.js
@@ -3,7 +3,7 @@ const Schema=mongoose.Schema
 const car=new Schema({
     platenumber: { type: String, required: true },
     brand: { type: String, required: true },
-    state: { type: String, required: true },
+    state: { type: Number, required: true },
     dailyvalue: { type: Number, required: true },
 })
 export const modeloCar=mongoose.model("cars",car)
